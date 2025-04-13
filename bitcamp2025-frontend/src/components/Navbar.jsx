e722f76508242edc04a989d97eb030c465eb986e
Kathy Chen <Kathychen331@outlook.com>
2025-04-13 03:28:57 -0400

    taskListPage start

--- bitcamp2025-frontend/src/components/Navbar.jsx
+++ bitcamp2025-frontend/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>MyApp</div>
+      <button className={styles.button} onClick={() => navigate(-1)}>Back</button>
+      <div className={styles.mograFont}>The Nest</div>
       <div className={styles.links}>
         <Link to="/" className={styles.link}>Home</Link>
         <Link to="/login" className={styles.link}>Login</Link>
--- bitcamp2025-frontend/src/pages/TasklistPage/TasklistPage.jsx
+++ bitcamp2025-frontend/src/pages/TasklistPage/TasklistPage.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./TasklistPage.module.css";
-
+import Navbar from "../../components/Navbar";
 //simple Tasklist page
 const TasklistPage = () => {
   const navigate = useNavigate();
@@ -15,12 +15,29 @@ const TasklistPage = () => {
     navigate("/edittask");
   };
 
+  
+
   return (
     <div className={styles.container}>
+      <Navbar />
+      <div className={styles.Left}>
+        <div className={styles.buttonContainer}>
+          <button onClick={handleClickTaskCreation} className={styles.imageButton}>
+            Create Task
+          </button>
+          <button onClick={handleClickEditTask} className={styles.imageButton}>
+            Edit Task
+          </button>
+        </div>
+      </div>
+
+
+      <div className={styles.Right}>
+        
+
+      </div>
       <h1>Task List Page</h1>
-      <button onClick={handleClickTaskCreation}>Create Task</button>
-      <button onClick={handleClickEditTask}>Edit Task</button>
-      <button onClick={() => navigate(-1)}>Back</button>
+   
       <div className={styles.taskList}>
         <h2>Your Tasks:</h2>
         <ul>
