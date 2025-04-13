2c42f67072b51b120b0530a48fb9d2772816f290
Abdul <abdulahad2004@yahoo.com>
2025-04-13 01:27:08 -0400

    Update schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -13,7 +13,8 @@ CREATE TABLE User (
 ------------------------------------------------------------------------------
 CREATE TABLE Household (
     household_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name         TEXT NOT NULL
+    name         TEXT NOT NULL,
+    group_id     TEXT NOT NULL,  -- e.g. random num for users to join household
 );
 
 ------------------------------------------------------------------------------
