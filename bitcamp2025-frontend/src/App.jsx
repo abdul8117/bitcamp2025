d267371c33262aa3ba62648d997004cef5a77091
Abdul <abdulahad2004@yahoo.com>
2025-04-12 20:11:46 -0400

    Landing page

--- bitcamp2025-frontend/src/App.jsx
+++ bitcamp2025-frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TitlePage from './pages/TitlePage/TitlePage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import SignupPage from './pages/SignupPage/SignupPage';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TitlePage from "./pages/TitlePage/TitlePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<TitlePage />} />
         <Route path="/login" element={<LoginPage />} />
--- bitcamp2025-frontend/src/pages/TitlePage/TitlePage.jsx
+++ bitcamp2025-frontend/src/pages/TitlePage/TitlePage.jsx
@@ -1,28 +1,55 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./TitlePage.module.css";
 
+// Import Bootstrap globally in your index.js (not here)
+import duck from "../../assets/duck.svg";
+import yellowDuckling from "../../assets/yellowduckling.svg";
+import bird from "../../assets/bird.svg";
 
-
-
-import { useNavigate } from 'react-router-dom';
-import styles from './TitlePage.module.css';
-
-
-// this is the title page this is what the user first sees
-// make a button that when clicked, it will redirect to the login page
-// make a button that when clicked, it will redirect to the signup page
-// Titlepage style is in the same folder
-
-const TitlePage = () => {
+function TitlePage() {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to Our App</h1>
-      <div className={styles.buttonGroup}>
-        <button className={styles.button} onClick={() => navigate('/login')}>Login</button>
-        <button className={styles.button} onClick={() => navigate('/signup')}>Sign Up</button>
+    <div className={styles.landingContainer}>
+      {/* TOP SECTION */}
+      <div className="text-center mt-5">
+        {/* Big stylized title using Bootstrap's display class */}
+        <h1
+          className={`display-3 text-success ${styles.mograFont}`}
+          style={{ fontSize: "8rem" }}
+        >
+          ~The Nest~
+        </h1>
+
+        {/* Buttons side by side */}
+        <div className="d-inline-flex gap-3 mt-3">
+          <button className="btn btn-outline-success btn-lg">
+            <p className={styles.mograFont} style={{ margin: 0 }}>
+              login
+            </p>
+          </button>
+          <button className="btn btn-outline-success btn-lg">
+            <p className={styles.mograFont} style={{ margin: 0 }}>
+              sign up
+            </p>
+          </button>
+        </div>
+      </div>
+
+      {/* BOTTOM BAR */}
+      <div className="bg-success d-flex justify-content-center align-items-center p-3">
+        <img src={duck} alt="Duck" className={styles.animal} />
+        <img
+          src={yellowDuckling}
+          alt="Yellow Duckling"
+          className={styles.animal}
+        />
+        {/* For the last one, we might not want margin-right */}
+        <img src={bird} alt="Bird" style={{ maxHeight: "80px" }} />
       </div>
     </div>
   );
-};
+}
 
 export default TitlePage;
