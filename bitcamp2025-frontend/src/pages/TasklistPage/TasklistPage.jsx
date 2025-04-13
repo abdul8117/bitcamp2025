43945120af0fab7044e0125d35e8c58d6ddcbe90
Kathy Chen <Kathychen331@outlook.com>
2025-04-13 04:39:14 -0400

    Tasklist Page

--- bitcamp2025-frontend/src/pages/TasklistPage/TasklistPage.jsx
+++ bitcamp2025-frontend/src/pages/TasklistPage/TasklistPage.jsx
@@ -2,15 +2,40 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./TasklistPage.module.css";
 import Navbar from "../../components/Navbar";
+import duckling from "../../assets/yellowduckling.svg";
+import dove from "../../assets/bird.svg";
+import swan from "../../assets/duck.svg";
 
 const TasklistPage = () => {
   const navigate = useNavigate();
+  //useState for the pet
+  const [userpet, setuserPet] = useState(null);
+  //list of household pets by fetching so start with empty array
+  const [houseHoldPet, setHouseHoldPet] = useState([]);
+
+
   const [tasks, setTasks] = useState([
     { id: 1, name: "Task 1", completed: false },
     { id: 2, name: "Task 2", completed: false },
     { id: 3, name: "Task 3", completed: false },
   ]);
 
+  const handleFetchUserPet = () => {
+    // Fetch user's pet from backend
+    //if fetched pet is =="duckling", "dove", "swan" then show the corresponding SVG
+    setuserPet("duckling");
+    // setPet("dove");
+    // setPet("swan");
+
+  };
+
+  const handleHouseHoldPet = () => {
+    // Fetch user's pet from backend
+    //if fetched pet is =="duckling", "dove", "swan" then show the corresponding SVG
+    // setPet("dove");
+    // setPet("swan");
+
+  };
   const handleClickTaskCreation = () => {
     navigate("/taskcreation");
   };
@@ -75,9 +100,20 @@ const TasklistPage = () => {
         </div>
 
         <div className={styles.Right}>
-          <h1>Task List Page</h1>
+          <h1 className={styles.title}>Task List Page</h1>
+
+          <div className={styles.Nest}>
+            <img
+              src={userpet === "duckling" ? duckling : userpet === "dove" ? dove : swan}
+              alt="Pet"
+              className={styles.petImage }
+            />
+          </div>
+          
+
+
         </div>
-        
+
         </div>
 
       </div>
