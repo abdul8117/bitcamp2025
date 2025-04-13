67ca24dbc2f2c734595f6b259bca8a52013a5e70
Kathy Chen <Kathychen331@outlook.com>
2025-04-13 01:37:46 -0400

    slider somewhat work

--- bitcamp2025-frontend/src/pages/GroupoptionPage/GroupoptionPage.jsx
+++ bitcamp2025-frontend/src/pages/GroupoptionPage/GroupoptionPage.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import styles from "./GroupoptionPage.module.css";
 import makeGroupImg from "../../assets/makegroup.svg"; // replace with your actual image paths
 import joinGroupImg from "../../assets/joingroup.svg";
+import Slider from "../../components/Slider";
 
 const GroupoptionPage = () => {
   const navigate = useNavigate();
@@ -35,6 +36,10 @@ const GroupoptionPage = () => {
       </div>
 
       <div className ={styles.Right}>
+        <div className ={styles.sliderContainer}>
+        <Slider />
+        </div>
+        
         
       </div>
       <h1 className={styles.title}>Group Option Page</h1>
